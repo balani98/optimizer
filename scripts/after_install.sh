0b61b6f0e138967b8040220c6d959715a6105678
deepanshubalani1998 <deepanshu.balani@xmedia.com>
2023-02-05 19:24:43 +0530

    yml files for CI/CD pushed

---
+++ scripts/after_install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# kill any servers that may be running in the background 
+sudo pkill -f runserver
+
+# kill frontend servers if you are deploying any frontend
+# sudo pkill -f tailwind
+# sudo pkill -f node
+
+cd /var/www/optimizer
+
+# activate virtual environment
+python3 -m venv venv
+source venv/bin/activate
+
+install requirements.txt
+pip install -r /var/www/optimizer/requirements.txt
+
+# restart apache2 server
+sudo systemctl restart apache2
