291954e43e73de3353c3d62e52fbb65ccb027c4f
Deepanshu Balani <deepanshu.balani@brainlabsdigital.com>
2024-02-21 19:26:43 +0530

    reversal

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,24 +3,23 @@
 # kill any servers that may be running in the background 
 sudo pkill -f runserver
 
+# stop the apache2 server
+sudo systemctl stop apache2
+
 # kill frontend servers if you are deploying any frontend
 # sudo pkill -f tailwind
 # sudo pkill -f node
 
-cd /mnt/apps/optimizer/source
+cd /var/www/optimizer
 
-# activate virtual environment
-python3 -m venv venv
-source venv/bin/activate
+
 
 # install requirements.txt
-pip install -r /mnt/apps/optimizer/source/requirements.txt
-
-# installing the cryptography for getting SSL modules
+pip install -r /var/www/optimizer/requirements.txt
+# installing the cryptography for SSL modules
 pip install cryptography==38.0.4
+# Declaring the environment variables
+sudo export ENVIRONMENT=production
 
-# restart apache2 server for production server
-# sudo systemctl restart apache2
-
-# restart python server 
-screen -dm bash -c  'nohup python manage.py runserver 0.0.0.0:8080' 
+# restart apache2 server
+sudo systemctl restart apache2
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -16,7 +16,5 @@ sudo ./install auto
 sudo apt-get update
 sudo apt-get install -y python3 python3-dev python3-pip python3-venv
 pip install --user --upgrade virtualenv
-
 # delete app
-# sudo rm -rf /mnt/apps/optimize/source
-
+# sudo rm -rf /var/www/optimizer
