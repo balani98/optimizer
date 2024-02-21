d09adae86181bb50745bf36812cb462de5f5e766
Deepanshu Balani <deepanshu.balani@brainlabsdigital.com>
2024-02-21 19:34:36 +0530

    reversal of files for test CI-CD

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,23 +3,24 @@
 # kill any servers that may be running in the background 
 sudo pkill -f runserver
 
-# stop the apache2 server
-sudo systemctl stop apache2
-
 # kill frontend servers if you are deploying any frontend
 # sudo pkill -f tailwind
 # sudo pkill -f node
 
-cd /var/www/optimizer
+cd /mnt/apps/optimizer/source
 
-
+# activate virtual environment
+python3 -m venv venv
+source venv/bin/activate
 
 # install requirements.txt
-pip install -r /var/www/optimizer/requirements.txt
-# installing the cryptography for SSL modules
-pip install cryptography==38.0.4
-# Declaring the environment variables
-sudo export ENVIRONMENT=production
+pip install -r /mnt/apps/optimizer/source/requirements.txt
 
-# restart apache2 server
-sudo systemctl restart apache2
+# installing the cryptography for getting SSL modules
+pip install cryptography==38.0.4
+
+# restart apache2 server for production server
+# sudo systemctl restart apache2
+
+# restart python server 
+screen -dm bash -c  'nohup python manage.py runserver 0.0.0.0:8080' 
